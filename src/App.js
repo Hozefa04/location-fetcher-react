f1010f117b7ea5c15e028fa1657d8b9bfb3b9f9d
Hozefa04 <hozefa.mobilefirst@gmail.com>
2022-10-21 10:53:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+var geolocation = require("geolocation");
 
 function App() {
+  const [latlong, setLatLong] = useState({
+    lat: "",
+    long: "",
+  });
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    geolocation.getCurrentPosition(function (err, position) {
+      if (err) {
+        setError(
+          "You have denied location access. Please turn on location from settings"
+        );
+        throw err;
+      }
+      console.log(position);
+      setLatLong({
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {latlong.lat === "" && error === "" ? (
+          <p>Provide location access</p>
+        ) : error !== "" ? (
+          <p> {error}</p>
+        ) : (
+          <>
+            <p>Lat: {latlong.lat} </p>
+            <p>Long: {latlong.long} </p>
+          </>
+        )}
       </header>
     </div>
   );
