b2738cdb822ef2d8b445d051764ba43215baab5e
Hozefa04 <hozefa.mobilefirst@gmail.com>
2022-10-21 12:53:01 +0530

    self

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   const openInNewTab = (lat, long) => {
     window.open(
       `https://location-demo.netlify.app?para1=${lat}&para2=${long}`,
-      "_blank",
+      "_self",
       "noopener,noreferrer"
     );
   };
