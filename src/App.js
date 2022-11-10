70466c920059f5702e74bd9d86530041eadcd852
Hozefa04 <hozefa.mobilefirst@gmail.com>
2022-11-10 19:21:00 +0530

    browser check added

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { browserName } from "react-device-detect";
 import appLogo from "./images/appicon.png";
 
 function App() {
-  const [timeLeft, setTimeLeft] = useState(null);
-  const [message, setMessage] = useState("Waiting for location access...");
-  // const [latlong, setLatLong] = useState({
-  //   lat: "",
-  //   long: "",
-  // });
-
   useEffect(() => {
-    setTimeLeft(3);
-
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log("Latitude is :", position.coords.latitude);
       console.log("Longitude is :", position.coords.longitude);
-
-      // setLatLong({
-      //   lat: position.coords.latitude,
-      //   long: position.coords.longitude,
-      // });
-
       openInNewTab(position.coords.latitude, position.coords.longitude);
     });
   }, []);
 
-  useEffect(() => {
-    if (timeLeft === 0) {
-      console.log("TIME LEFT IS 0");
-      setTimeLeft(null);
-      setMessage("Please switch to Chrome browser for a better experience.");
-    }
-
-    // exit early when we reach 0
-    if (!timeLeft) return;
-
-    // save intervalId to clear the interval when the
-    // component re-renders
-    const intervalId = setInterval(() => {
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
-
-    // clear interval on re-render to avoid memory leaks
-    return () => clearInterval(intervalId);
-    // add timeLeft as a dependency to re-rerun the effect
-    // when we update it
-  }, [timeLeft]);
-
   const openInNewTab = (lat, long) => {
     window.open(
-      `https://now.playshotcaller.com/?para1=${lat}&para2=${long}`,
+      `https://now.playshotcaller.com/?para1=${lat}&para2=${long}&browserName=${browserName}`,
       "_self",
       "noopener,noreferrer"
     );
@@ -60,7 +24,13 @@ function App() {
     <div className="App">
       <center>
         <img src={appLogo} alt="new" />
-        <p className="note">{message}</p>
+        {browserName === "Chrome" ? (
+          <p className="note">Please allow location access.</p>
+        ) : (
+          <p className="note">
+            Please switch to chrome for a better experience.
+          </p>
+        )}
       </center>
     </div>
   );
