b6d45484fc09c56f7d526092171a295c79e69fd3
Hozefa04 <hozefa.mobilefirst@gmail.com>
2022-11-15 15:57:17 +0530

    popup check

--- src/App.js
+++ src/App.js
@@ -8,13 +8,29 @@ function App() {
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log("Latitude is :", position.coords.latitude);
       console.log("Longitude is :", position.coords.longitude);
-      openInNewTab(position.coords.latitude, position.coords.longitude);
+
+      var windowName = "popup check";
+      var popUp = window.open(
+        "http://www.google.in",
+        windowName,
+        "width=1, height=1, left=0, top=0"
+      );
+      if (popUp == null || typeof popUp == "undefined") {
+        openInNewTab(position.coords.latitude, position.coords.longitude, true);
+      } else {
+        popUp.close();
+        openInNewTab(
+          position.coords.latitude,
+          position.coords.longitude,
+          false
+        );
+      }
     });
   }, []);
 
-  const openInNewTab = (lat, long) => {
+  const openInNewTab = (lat, long, popup) => {
     window.open(
-      `https://now.playshotcaller.com/?para1=${lat}&para2=${long}&browserName=${browserName}`,
+      `https://now.playshotcaller.com/?para1=${lat}&para2=${long}&browserName=${browserName}&isPopupBlocked=${popup}`,
       "_self",
       "noopener,noreferrer"
     );
