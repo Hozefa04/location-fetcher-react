628358ae9ecabbfa53ff9f35ddf3d9ac048e12b0
Hozefa04 <hozefa.mobilefirst@gmail.com>
2022-11-29 10:41:32 +0530

    store integration done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { browserName } from "react-device-detect";
 import appLogo from "./images/appicon.png";
+import appBg from "./images/bg.png";
+import appStoreLogo from "./images/appstore.png";
+import playStoreLogo from "./images/play.png";
 
 function App() {
   useEffect(() => {
@@ -16,17 +19,43 @@ function App() {
         "width=1, height=1, left=0, top=0"
       );
       if (popUp == null || typeof popUp == "undefined") {
-        // openInNewTab(position.coords.latitude, position.coords.longitude, true);
+        openInNewTab(position.coords.latitude, position.coords.longitude, true);
       } else {
         popUp.close();
-        // openInNewTab(
-        //   position.coords.latitude,
-        //   position.coords.longitude,
-        //   false
-        // );
+        openInNewTab(
+          position.coords.latitude,
+          position.coords.longitude,
+          false
+        );
       }
     });
   }, []);
+
+  /**
+   * Determine the mobile operating system.
+   * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+   *
+   * @returns {String}
+   */
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+    }
+
+    return "unknown";
+  }
 
   const openInNewTab = (lat, long, popup) => {
     window.open(
@@ -37,11 +66,27 @@ function App() {
   };
 
   return browserName === "Chrome" ? (
-    <div className="App background"></div>
+    <div className="App">
+      <div className="background">
+        {/* <div className="storeLogo"> */}
+        <img className="bg" src={appBg} alt="store" />
+        {getMobileOperatingSystem() === "iOS" ? (
+          <a href="https://apps.apple.com/in/app/shotcaller-make-sports-picks/id1558033440">
+            <img className="store" src={appStoreLogo} alt="" />
+          </a>
+        ) : (
+          <a href="https://play.google.com/store/apps/details?id=com.app.shotcaller">
+            <img className="store" src={playStoreLogo} alt="" />
+          </a>
+        )}
+
+        {/* </div> */}
+      </div>
+    </div>
   ) : (
     <div className="App">
       <center>
-        <img src={appLogo} alt="new" />
+        <img className="logo" src={appLogo} alt="new" />
         <p className="note">Please switch to chrome for a better experience.</p>
       </center>
     </div>
